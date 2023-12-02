c2232fed74303c439dd56c27032fe767ca3abb27
Edson <edsoneurides0705@gmail.com>
2023-12-01 21:22:26 -0300

    Modelo de comentário

---
+++ src/main/java/com/ecommerce/ecommerce/Models/Comment.java
@@ -0,0 +1,25 @@
+package com.ecommerce.ecommerce.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "t_comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userName;
+    @Column(name="comment")
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
