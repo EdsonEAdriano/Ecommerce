83987a03349519270d9832a3fb92275bc1781f24
Edson <edsoneurides0705@gmail.com>
2023-12-01 21:22:18 -0300

    Modelo de compra

---
+++ src/main/java/com/ecommerce/ecommerce/Models/Buy.java
@@ -0,0 +1,20 @@
+package com.ecommerce.ecommerce.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "t_buy")
+public class Buy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Date date;
+    private String buyer;
+    private Double totalPrice;
+}
