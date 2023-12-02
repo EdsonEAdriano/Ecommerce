7aac827b353c934a94a0ce7da8a6c1b570251086
Edson <edsoneurides0705@gmail.com>
2023-12-01 21:23:24 -0300

    Modelo de produto

---
+++ src/main/java/com/ecommerce/ecommerce/Models/Product.java
@@ -0,0 +1,26 @@
+package com.ecommerce.ecommerce.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "t_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+    private Double price;
+    private String evaluation;
+    @OneToMany(mappedBy = "product")
+    private List<Image> images;
+}
