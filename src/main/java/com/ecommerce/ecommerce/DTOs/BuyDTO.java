877048b7e7e061892ba9e70be62e2a239dd8b1d5
Edsu <edsoneurides0705@gmail.com>
2023-12-03 23:40:40 -0300

    Criação modelo DTO de compra

---
+++ src/main/java/com/ecommerce/ecommerce/DTOs/BuyDTO.java
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerce.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BuyDTO {
+    private String buyer;
+    private Double totalPrice;
+}
