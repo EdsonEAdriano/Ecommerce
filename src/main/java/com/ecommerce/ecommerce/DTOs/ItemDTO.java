60142c188a32c10bc594e9568858c9d2db814db1
Edsu <edsoneurides0705@gmail.com>
2023-12-03 23:34:12 -0300

    Criação do modelo DTO item

---
+++ src/main/java/com/ecommerce/ecommerce/DTOs/ItemDTO.java
@@ -0,0 +1,12 @@
+package com.ecommerce.ecommerce.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemDTO {
+    private Integer productID;
+    private Integer buyID;
+    private Integer quantity;
+}
