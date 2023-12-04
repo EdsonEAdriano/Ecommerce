414a957926bc6d8ed0be294ba133323ecccb5cac
Edsu <edsoneurides0705@gmail.com>
2023-12-03 23:37:04 -0300

    Criação modelo DTO imagem

---
+++ src/main/java/com/ecommerce/ecommerce/DTOs/ImageDTO.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce.DTOs;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImageDTO {
+    private Integer productID;
+    private Integer sequence;
+    private String url;
+}
