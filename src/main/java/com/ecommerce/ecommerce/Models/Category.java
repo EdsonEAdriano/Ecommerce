03f239f8d03c9537091adc97d0ae65e464f5dfcf
Edsu <edsoneurides0705@gmail.com>
2023-12-04 23:59:13 -0300

    Construtores para modelo categoria

--- src/main/java/com/ecommerce/ecommerce/Models/Category.java
+++ src/main/java/com/ecommerce/ecommerce/Models/Category.java
@@ -1,9 +1,13 @@
 package com.ecommerce.ecommerce.Models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
