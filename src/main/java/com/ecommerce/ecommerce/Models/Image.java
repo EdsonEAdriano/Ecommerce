4deeae0adad9d71f9d73b55cd61e48db6e7dfa6c
Edsu <edsoneurides0705@gmail.com>
2023-12-06 00:19:16 -0300

    Anotações de contrutores e não gerar looping na criação de Imagem

--- src/main/java/com/ecommerce/ecommerce/Models/Image.java
+++ src/main/java/com/ecommerce/ecommerce/Models/Image.java
@@ -1,9 +1,15 @@
 package com.ecommerce.ecommerce.Models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -12,6 +18,7 @@ public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
